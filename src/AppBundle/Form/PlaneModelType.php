612d038b4d1e6a0384625a6274d4ee2ed03aa582
Aurelle Martin <martinaurelle.ma@gmail.com>
2018-04-23 17:54:04 +0200

    martin

--- src/AppBundle/Form/PlaneModelType.php
+++ src/AppBundle/Form/PlaneModelType.php
@@ -13,7 +13,7 @@ class PlaneModelType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('model')->add('manufacturer')->add('cruiseSpeed')->add('planeNbSeats');
+        $builder->add('isAvailable')->add('model')->add('manufacturer')->add('cruiseSpeed')->add('planeNbSeats');
     }/**
      * {@inheritdoc}
      */
